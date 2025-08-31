5af6e8e473605d9fb884df890f71bcb9b88e5f8e
hgkim7 <hgkim2738@gmail.com>
2025-08-31 17:33:03 +0900

    f

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back is stock" do
+    product = products(:tshirt)
+
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
   # test "the truth" do
   #   assert true
   # end
